f3fe8f68cd98a40970c1fe34b1532adfa5481e32
yashpatil4904 <148094168+yashpatil4904@users.noreply.github.com>
2025-05-28 03:55:42 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,15 +3,20 @@ import dotenv from "dotenv";
 import {initDB} from "./config/db.js"
 import ratelimiter from "./middleware/ratelimiter.js";
 import trasaction from "./routes/transaction.js"
-
+import job  from "./config/cron.js"
 dotenv.config();
+
 const PORT =process.env.PORT ||5001;
 const app = express();
+if(process.env.NODE_ENV === "production") job.start();
 app.use(ratelimiter);
 app.use(express.json());
 // app.get("/",(req,res)=>{
 //     res.send("it's working");
 // })
+app.get("/api/health",(req,res)=>{
+    res.status(200).json({status : "OK"});
+});
 app.use("/api/transaction",trasaction);
 
 initDB().then(()=>{
